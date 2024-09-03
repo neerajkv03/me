faec3a34b0a2fa813e946cdc0bbff781c2682eba
itsme <neerajkv03@gmail.com>
2024-09-03 19:10:02 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex w-screen h-screen items-center justify-center">
+      <h1 className="font-mono">the3coders.com</h1>
+    </main>
+  );
+}
