fb9a8e9642d784194a05baa119f21c3c3179704a
itsme <neerajkv03@gmail.com>
2024-09-03 19:16:27 +0530

    started cooking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex w-screen h-screen items-center justify-center">
-      <h1 className="font-mono">the3coders.com</h1>
+      <div className="font-mono font-bold text-center">
+        <h1>the3coders.com</h1>
+        <h1>there is something cooking!!!</h1>
+        <h1 className="text-5xl mt-5">🍳</h1>
+      </div>
     </main>
   );
 }
